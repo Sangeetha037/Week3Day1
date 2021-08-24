0d632fc110ed9bb12f792678cc213bddedab9639
Sangeetha037 <88918740+Sangeetha037@users.noreply.github.com>
2021-08-24 20:40:17 +0530

    Week3Day1_Assignment

---
+++ Student.java
@@ -0,0 +1,33 @@
+package week3.day1.assignment;
+
+public class Student extends College{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student access = new Student();
+		access.collegeName();
+		access.collegeCode();
+		access.collegeRank();
+		access.deptName();
+		access.studentName();
+		access.studentDept();
+		access.studentId();
+		
+	}
+
+	public void studentName() {
+		// TODO Auto-generated method stub
+		System.out.println("Student Class_studentName is the method");
+	}
+	public void studentDept() {
+		// TODO Auto-generated method stub
+		System.out.println("Student Class_studentDept is the method");
+	}
+	
+	public void studentId() {
+		// TODO Auto-generated method stub
+		System.out.println("Student Class_studentId is the method");
+	}
+}
+
+
