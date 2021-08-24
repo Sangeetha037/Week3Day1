cce2a7942a59db445ec21c02c38f7d4ab74ac3c9
Sangeetha037 <88918740+Sangeetha037@users.noreply.github.com>
2021-08-24 20:40:00 +0530

    Week3Day1_Assignment

---
+++ Department.java
@@ -0,0 +1,8 @@
+package week3.day1.assignment;
+
+public class Department {
+	public void deptName() {
+		// TODO Auto-generated method stub
+ System.out.println("Department Class_collegeName Method");
+	}
+}
