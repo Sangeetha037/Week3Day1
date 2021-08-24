7932102fe58b7c4bf11c2cd4ac0ae080c88fbe2c
Sangeetha037 <88918740+Sangeetha037@users.noreply.github.com>
2021-08-24 18:11:40 +0530

    Week3Day1_Assignment

---
+++ Desktop.java
@@ -0,0 +1,18 @@
+package week3.day1.assignment;
+
+public class Desktop extends Computer {
+
+	public void desktopSize() {
+		// TODO Auto-generated method stub
+		System.out.println("Desktop Class_Desktop size is method");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Desktop ext = new Desktop();
+		ext.computerModel();
+		ext.desktopSize();
+
+	}
+
+}
