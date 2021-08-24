d448365fef2afe672e8e8385d071610504ed62ce
Sangeetha037 <88918740+Sangeetha037@users.noreply.github.com>
2021-08-24 18:11:22 +0530

    Week3Day1_Assignment

---
+++ Computer.java
@@ -0,0 +1,9 @@
+package week3.day1.assignment;
+
+public class Computer {	
+
+	public void computerModel() {
+		// TODO Auto-generated method stub
+System.out.println("Computer Class_Computer Model method ");
+	}
+}
