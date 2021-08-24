32a67c55cb90501bda5e4f3a79c7d022b5f3b398
Sangeetha037 <88918740+Sangeetha037@users.noreply.github.com>
2021-08-24 20:38:56 +0530

    Week3Day1_Assignment

---
+++ College.java
@@ -0,0 +1,20 @@
+package week3.day1.assignment;
+
+public class College extends Department{
+	
+	public void collegeName() {
+		// TODO Auto-generated method stub
+ System.out.println("College Class_collegeName Method");
+	}
+	
+	public void collegeCode() {
+		// TODO Auto-generated method stub
+ System.out.println("College Class_collegeCode Method");
+	}
+	
+	public void collegeRank() {
+		// TODO Auto-generated method stub
+ System.out.println("College Class_collegeRank Method");
+	}	
+	
+}
